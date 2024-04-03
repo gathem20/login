ae3553dcdf939606079f7e0d71853d92d80a98c7
gathem20 <109413363+gathem20@users.noreply.github.com>
2024-04-03 23:09:27 +0200

    Update course.js

--- course.js
+++ course.js
@@ -4,8 +4,28 @@ const express = require("express");
 const router = express.Router();
 const Course = require("./dbConnect");
 
+//start post
 
+app.post("/post", async (req, res) => {
+  try {
+    const { name, description, image } = req.body;
+    // const uploadimage = await image_kit.upload({
+    //   file: image,
+    //   fileName: "",
+    // });
 
+    const data = await database.insertOne({
+      name,
+      description,
+      image,
+    });
+    console.log(data);
+    res.json({ name, description, image });
+  } catch (err) {
+    console.log("something wrong!", err);
+  }
+});
+//end post
 
 
 
